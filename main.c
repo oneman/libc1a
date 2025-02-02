40ec9999c616eb9176d5d28c26a44c4281f09548
David Richards <rawdod@gmail.com>
2025-02-02 02:57:22 -0500

    ok great lol

--- main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int i = 1, want = 2, make = 4;
-  return printf("%d = i, want = %d, make = %d\n", i, want, make);
-}
--- stdno.h
+++ stdno.h
@@ -1,6 +1,7 @@
 #include "0.h"
 #include "A.h"
 #include "B.h"
+#include "C.h"
 
 #include <unistd.h>
 #include <stdio.h>
